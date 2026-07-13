aade4288011624649f4479d46e0affbbf50d5ce8
Gustavo Sérgio <263528374+Gustavo-Faber@users.noreply.github.com>
2026-07-13 19:01:28 -0300

    Star

---
+++ calculator.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+class Application():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Calculator")
+
+        self.window.mainloop()
+
+Application()
